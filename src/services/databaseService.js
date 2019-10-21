567dbbc0d9cde9c689d81d754c91eea45cfd76d1
jaime <jgarciadiaz@groupon.com>
2019-10-21 06:34:46 -0500

    chore: adjust props

--- src/services/databaseService.js
+++ src/services/databaseService.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 function openDatabase(dbUrl) {
-  return mongoose.connect(dbUrl, { useMongoClient: true })
+  return mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 }
 
 module.exports.openDatabase = openDatabase
